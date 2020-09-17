8b1971cda1d9af8735369c89e62b05d9633523b6
Norwid Behrnd <nbehrnd@yahoo.com>
2020-09-17 13:59:20 +0000

    update dek_wikimedia_py to monitor tag properties
    Changes are a pick from branch dev, commit 712afa93f2d9.
    
    According to Thirunavukkarasye-Raveendran, the identification of
    illustrations about abbreviations, conjugations, and multiple forms
    of symbolisation may be helpful.

--- dek_wikimedia_lists.py
+++ dek_wikimedia_lists.py
@@ -5,7 +5,7 @@
 # author:  nbehrnd@yahoo.com
 # license: MIT 2020
 # date:    2020-09-10 (YYYY-MM-DD)
-# edit:    2020-09-14 (YYYY-MM-DD)
+# edit:    2020-09-17 (YYYY-MM-DD)
 #
 """Assistenzskript #5 zu Wikimedia Projekt zur DEK / Verkehrsschrift.
 
@@ -80,6 +80,25 @@
       |-
 
       Ablage ist Datei [wikimedia_thumbnail_groups.txt].
+
+    + Option -v listet die mehrfache Symbolisierungen von gleichen
+      langschriftlichen Formen (_v1, _v2, -_v3, -_v4) mit ihrer
+      Stammform.  Die Sortierung ist alphabetisch (a-z, ä, ö, ü) ohne
+      Berücksichtigung von Gross- oder Kleinschreibung.
+
+    + Option -a listed in alphabetischer Ordnung (a-z, ä, ö, ü; ohne
+      Berücksichtigung von Gross- oder Kleinschreibung) Abkürzungen
+      darstellende .svg.  Gegenwärtig sind das langschriftliche Formen
+      mit mehr als einem Punkt (wie 'd. h.'); oder Formen mit mehr als
+      einem Grossbuchstaben bei simultaner Abwesenheit von entweder
+      Bindestrich, Unterstrich, Leerzeichen, oder dem kontrastierenden
+      String 'ABER'.
+
+    + Option -c listed die Dateien, die die Pronomina ich, du, er, sie
+      es, wir, ihr enthalten.  Die Gross- und Kleinschreibung spielt
+      weder bei der Identifizierung eine Rolle (auch 'Sie' gilt als
+      'sie', 'Ihr´ als 'ihr'), noch bei der Sortierung der Dateinamen
+      (a-z, ä, ö, ü).
 
     Stets beginnt die Ausgabe mit einem vierzeiligen Kopf, der den
     Namen der Datei, das Erstellungsdatum und die Anzahl der aus
@@ -240,6 +259,216 @@ def output_grouped_preview():
         sys.exit()
 
 
+def variant_symbolizations():
+    """List the raw_data files drawn in multiple variants.
+
+    Considered are variants explicitly labeled by _v1, _v2, _v3, and
+    _v4.  In addition, for a variant .svg named like 'example_v2.svg',
+    the script equally suggests the complement file 'example.svg'."""
+    variant_register = []
+    today = date.today()
+
+    # learn from the past:
+    for entry in register:
+        # skip plausible commentary lines:
+        if str(entry).startswith("#"):
+            continue
+
+        if str(entry)[-7:] in ["_v1.svg", "_v2.svg", "_v3.svg", "_v4.svg"]:
+            variant = str(entry).split("DEK")[1]
+            variant = ''.join(["DEK", variant])
+
+            # Recreate accents and umlauts from the internet notation:
+            variant = unquote(str(variant))
+
+            variant_register.append(str(variant))
+
+        # .svg are marked by "_v2.svg", but "_v1.svg" marks may be incomplete:
+        if str("_v2.svg") in str(entry):
+            complement_file = str(entry).split("DEK")[1]
+            complement_file = ''.join(["DEK", complement_file])
+
+            # replace "_v2.svg" by ".svg"
+            complement_file = complement_file.split("_v2.svg")[0]
+            complement_file = ''.join([complement_file, ".svg"])
+
+            # Recreate accents and umlauts from the internet notation:
+            complement_file = unquote(str(complement_file))
+
+            variant_register.append(str(complement_file))
+    variant_register.sort(key=str.lower)
+
+    # document findings:
+    try:
+        with open("wikimedia_variants.txt", mode="w") as newfile:
+            # header commentaries
+            newfile.write("# name: wikimedia_variants.txt\n")
+            newfile.write("# date: {}\n".format(today))
+            newfile.write("# data: {}\n#\n".format(len(variant_register)))
+
+            # and the entries:
+            for variant in variant_register:
+                output = ""
+                for char in str(variant):
+                    if str(char) == str("_"):
+                        output += str(" ")
+                    else:
+                        output += str(char)
+                newfile.write("{}\n".format(str(output)))
+
+        print("File 'wikimedia_variants.txt' was written.")
+
+    except IOError:
+        print("Error writing file 'wikimedia_variants.txt'.  Exit.")
+        sys.exit()
+
+
+def check_abbreviations():
+    """Identify and report symbolizations about abbreviations.
+
+    Entries are considered to be about an abbreviations if either a)
+    the stem contains more than one period, like in 'e.g.', 'd. M.'.
+    In addition b), if the stem does not contain a blank ' ' while
+    more than one character is upper case, like in 'UNESCO'.  Again,
+    the list will be sorted a-z, ä, ö, ü without discern for upper /
+    lower case spelling."""
+
+    abbreviation_register = []
+    today = date.today()
+
+    # learn from the past:
+    for entry in register:
+        # skip plausible commentary lines:
+        if str(entry).startswith("#"):
+            continue
+
+        file = str(entry).split("DEK")[1]
+        file = ''.join(["DEK", file])
+
+        # Recreate accents and umlauts from the internet notation:
+        file = unquote(str(file))
+
+        # identify the stem:
+        stem = str(file).split("_-_")[-1]
+        stem = str(stem).split(".svg")[0]
+
+        # test for periods:
+        count_periods = 0
+        for char in str(stem):
+            if str(char) is str("."):
+                count_periods += 1
+        if count_periods > 1:
+            abbreviation_register.append(file)
+
+        # check for upper case characters:
+        if (str("ABER") in str(stem)) or (str("_") in str(stem)) or (
+                str("-") in str(stem)) or (str(" ") in str(stem)):
+            pass
+        else:
+            count_upper_case = 0
+            for char in str(stem):
+                if str(char).isupper():
+                    count_upper_case += 1
+            if count_upper_case > 1:
+                abbreviation_register.append(file)
+    abbreviation_register.sort(key=str.lower)
+
+    # report the findings:
+    try:
+        with open("wikimedia_abbreviations.txt", mode="w") as newfile:
+            # header comments
+            newfile.write("# name: wikimedia_abbreviations.txt\n")
+            newfile.write("# date: {}\n".format(today))
+            newfile.write("# data: {}\n#\n".format(len(abbreviation_register)))
+
+            for abbreviation in abbreviation_register:
+                output = ""
+                for char in str(abbreviation):
+                    if str(char) == str("_"):
+                        output += str(" ")
+                    else:
+                        output += str(char)
+                newfile.write("{}\n".format(str(output)))
+
+        print("File 'wikimedia_abbreviations.txt' was written.")
+
+    except IOError:
+        print("Error writing 'wikimedia_abbreviations.txt'.  Exit.")
+        sys.exit()
+
+
+def check_conjugations():
+    """Identify symbolizations likely about conjugations.
+
+    A symbolization about conjugation consist of two or more words,
+    separated in the file name by an underscore.  In addition, it must
+    contain one of the following strings:  'ich_', 'du_', 'er_',
+    'sie_', 'es_', 'wir_' or 'ihr_'."""
+
+    conjugation_register = []
+    today = date.today()
+
+    # learn from the past:
+    for entry in register:
+        # skip plausible commentary lines:
+        if str(entry).startswith("#"):
+            continue
+
+        file = str(entry).split("DEK")[1]
+        file = ''.join(["DEK", file])
+
+        # Recreate accents and umlauts from the internet notation:
+        file = unquote(str(file))
+
+        # identify the stem:
+        stem = str(file).split("_-_")[-1]
+        stem = str(stem).split(".svg")[0]
+
+        # exclude explicit variant symbolizations
+        if str(stem)[-3:] in ("_v1", "_v2", "_v3", "_v4"):
+            continue
+        # exclude contrasting symbolizations:
+        if str("ABER") in str(stem):
+            continue
+        # exclude symbolizations with only one word:
+        if str("_") not in str(stem):
+            continue
+
+        # conjugation test
+        test_list = str(stem).split("_")
+        example_identified = False
+        for word in test_list:
+            if (example_identified is False) and (str(word).lower() in [
+                    'ich', 'du', 'er', 'sie', 'es', 'wir', 'ihr'
+            ]):
+                conjugation_register.append(file)
+                example_identified = True
+    conjugation_register.sort(key=str.lower)
+
+    # report the results:
+    try:
+        with open("wikimedia_conjugation.txt", mode="w") as newfile:
+            # commenting headers:
+            newfile.write("# name: wikimedia_conjugation.txt\n")
+            newfile.write("# date: {}\n".format(today))
+            newfile.write("# data: {}\n#\n".format(len(conjugation_register)))
+
+            for example in conjugation_register:
+                output = ""
+                for char in str(example):
+                    if str(char) == str("_"):
+                        output += str(" ")
+                    else:
+                        output += str(char)
+                newfile.write("{}\n".format(str(output)))
+
+        print("File 'wikimedia_conjugation.txt' was written.")
+
+    except IOError:
+        print("Error writing file 'wikimedia_conjugation.txt'.  Exit.")
+        sys.exit()
+
+
 # clarification for argparse, start:
 parser = argparse.ArgumentParser(
     description=
@@ -263,6 +492,21 @@ group.add_argument('-g',
                    action='store_true',
                    help='create the grouped list of previews')
 
+group.add_argument('-v',
+                   '--variants',
+                   action='store_true',
+                   help='print a list of .svg drawn in variants')
+
+group.add_argument('-a',
+                   '--abbreviations',
+                   action='store_true',
+                   help='list .svg about abbreviations')
+
+group.add_argument('-c',
+                   '--conjugations',
+                   action='store_true',
+                   help='list .svg about conjugations')
+
 args = parser.parse_args()
 # clarification for argparse, end.
 
@@ -278,3 +522,13 @@ if __name__ == "__main__":
     elif args.grouppreview:
         print("Seek the creation of a grouped previewlist.")
         output_grouped_preview()
+
+    elif args.variants:
+        print("Print a list of symbolizations drawn in variants.")
+        variant_symbolizations()
+
+    elif args.abbreviations:
+        check_abbreviations()
+
+    elif args.conjugations:
+        check_conjugations()
